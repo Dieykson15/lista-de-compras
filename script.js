13af0f65c104bee0ed7305d686d23e35d117b6ad
Dieykson15 <dieyksonartursjp@gmail.com>
2026-02-20 17:40:23 -0300

    Remove button and alert message

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const addItem = document.querySelector("form")
 const item = document.getElementById("choose-item")
 const listItem = document.querySelector("ul")
 const del = document.querySelectorAll(".delete")
+const footer = document.querySelector("footer")
+const remove = document.querySelector(".remove")
+const checkbox = document.querySelectorAll(".item")
 
 addItem.addEventListener("submit", (event) => {
   event.preventDefault()
@@ -33,8 +36,18 @@ function newItem() {
 listItem.addEventListener("click", (event) => {
   if (event.target.classList.contains("delete")) {
     event.target.parentElement.remove()
+    footer.classList.add("show-result")
+    setTimeout(function () {
+      footer.style.display = "none"
+    }, 3000)
+    footer.style = null
+    remove.addEventListener("click", (event) =>
+      footer.classList.remove("show-result"))
   }
 })
+
+
+
 
 
 
